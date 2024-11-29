1ccc4f64a1145109296e60e29982860877f322ae
Ellen McGrory <35548050+EllenMcG@users.noreply.github.com>
2024-11-29 22:02:24 +0000

    Fix path variable in weather.sh for Github actions

--- weather.sh
+++ weather.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 date
 echo "Weather data for Athenry is downloading..." 
-wget -O /workspaces/computer_infrastructure_assessment/data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
+wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
 echo "...Weather data for Athenry is downloaded" 
--- weather_updated.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-date
-echo "Weather data for Athenry is downloading..." 
-wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
-echo "...Weather data for Athenry is downloaded" 
